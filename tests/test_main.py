759975e644f084d8e30de4124ef18bcc031736d3
charismaticc <akbar20sharipov01@gmail.com>
2023-01-29 22:40:07 +0300

    Add tests to GH Actions

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,3 +6,30 @@ api_url = "http://localhost:8000"
 def test_healthcheck():
     response = requests.get(f"{api_url}/__health")
     assert response.status_code == 200
+
+
+class TestPhones:
+    def test_get_empty_phones(self):
+        response = requests.get(f"{api_url}/v1/phones")
+        assert response.status_code == 200
+        assert len(response.json()) == 0
+
+    def test_create_phones(self):
+        body = {'model': "Model", "developer": "developer"}
+        response = requests.post(f"{api_url}/v1/phones", json=body)
+        assert response.status_code == 200
+        assert response.json().get('model') == 'Model'
+        assert response.json().get('developer') == 'Developer'
+        assert response.json().get('id') == 0
+
+    def test_get_phones_by_id(self):
+        response = requests.get(f"{api_url}/v1/phones/0")
+        assert response.status_code == 200
+        assert response.json().get('model') == 'Model'
+        assert response.json().get('developer') == 'Developer'
+        assert response.json().get('id') == 0
+
+    def test_get_empty_phones(self):
+        response = requests.get(f"{api_url}/v1/phones")
+        assert response.status_code == 200
+        assert len(response.json()) == 1
